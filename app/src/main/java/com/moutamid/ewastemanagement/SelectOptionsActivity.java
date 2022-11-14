e4fb7ad2f2e03deb405fefc202fbd918dd221e42
suleman <sunnybajwa904@gmail.com>
2022-11-14 19:36:43 +0500

    Initial commit

---
+++ app/src/main/java/com/moutamid/ewastemanagement/SelectOptionsActivity.java
@@ -0,0 +1,14 @@
+package com.moutamid.ewastemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_options);
+    }
+}
