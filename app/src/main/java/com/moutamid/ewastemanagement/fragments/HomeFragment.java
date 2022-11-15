4db71a9ff40bc3c8cf516744312034db78120c07
suleman <sunnybajwa904@gmail.com>
2022-11-15 16:48:38 +0500

    Initial commit

--- app/src/main/java/com/moutamid/ewastemanagement/fragments/HomeFragment.java
+++ app/src/main/java/com/moutamid/ewastemanagement/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.moutamid.ewastemanagement.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,9 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.card.MaterialCardView;
 import com.moutamid.ewastemanagement.R;
+import com.moutamid.ewastemanagement.SearchActivity;
+import com.moutamid.ewastemanagement.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    MaterialCardView searchBtn;
+    FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,6 +27,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        binding.searchBtn.setOnClickListener(v -> {
+            startActivity(new Intent(view.getContext(), SearchActivity.class));
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
--- app/src/main/java/com/moutamid/ewastemanagement/fragments/MenuFragment.java
+++ app/src/main/java/com/moutamid/ewastemanagement/fragments/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.moutamid.ewastemanagement.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,9 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.moutamid.ewastemanagement.ProfileActivity;
 import com.moutamid.ewastemanagement.R;
+import com.moutamid.ewastemanagement.databinding.FragmentMenuBinding;
 
 public class MenuFragment extends Fragment {
+
+    FragmentMenuBinding binding;
 
     public MenuFragment() {
         // Required empty public constructor
@@ -20,6 +25,19 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        binding = FragmentMenuBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        binding.Profile.setOnClickListener(v -> {
+            startActivity(new Intent(view.getContext(), ProfileActivity.class));
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
