6ad9426326b4b37bdee6adb908b9d9198cad4471
suleman <sunnybajwa904@gmail.com>
2022-11-22 22:07:01 +0500

    commit

--- app/src/main/java/com/moutamid/ewastemanagement/SelectOptionsActivity.java
+++ app/src/main/java/com/moutamid/ewastemanagement/SelectOptionsActivity.java
@@ -9,6 +9,8 @@ import com.moutamid.ewastemanagement.databinding.ActivitySelectOptionsBinding;
 
 public class SelectOptionsActivity extends AppCompatActivity {
     ActivitySelectOptionsBinding binding;
+    String option = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,42 @@ public class SelectOptionsActivity extends AppCompatActivity {
         binding.submitBtn.setOnClickListener(v -> {
             startActivity(new Intent(this, DashboardActivity.class));
             finish();
+        });
+
+        binding.recycleBtn.setOnClickListener(v -> {
+            option = "Recycle";
+            binding.recycleBtn.setCardBackgroundColor(getResources().getColor(R.color.green));
+            binding.recycleTV.setTextColor(getResources().getColor(R.color.white));
+
+            binding.repairBtn.setCardBackgroundColor(getResources().getColor(R.color.white));
+            binding.repairTv.setTextColor(getResources().getColor(R.color.textblack));
+
+            binding.reuseBtn.setCardBackgroundColor(getResources().getColor(R.color.white));
+            binding.reuseTv.setTextColor(getResources().getColor(R.color.textblack));
+        });
+
+        binding.repairBtn.setOnClickListener(v -> {
+            option = "Repair";
+            binding.recycleBtn.setCardBackgroundColor(getResources().getColor(R.color.white));
+            binding.recycleTV.setTextColor(getResources().getColor(R.color.textblack));
+
+            binding.repairBtn.setCardBackgroundColor(getResources().getColor(R.color.green));
+            binding.repairTv.setTextColor(getResources().getColor(R.color.white));
+
+            binding.reuseBtn.setCardBackgroundColor(getResources().getColor(R.color.white));
+            binding.reuseTv.setTextColor(getResources().getColor(R.color.textblack));
+        });
+
+        binding.reuseBtn.setOnClickListener(v -> {
+            option = "Reuse";
+            binding.recycleBtn.setCardBackgroundColor(getResources().getColor(R.color.white));
+            binding.recycleTV.setTextColor(getResources().getColor(R.color.textblack));
+
+            binding.repairBtn.setCardBackgroundColor(getResources().getColor(R.color.white));
+            binding.repairTv.setTextColor(getResources().getColor(R.color.textblack));
+
+            binding.reuseBtn.setCardBackgroundColor(getResources().getColor(R.color.green));
+            binding.reuseTv.setTextColor(getResources().getColor(R.color.white));
         });
     }
 }
